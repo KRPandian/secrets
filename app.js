80b394c1436b46211eb1f227707c010b9d921725
Kanthasamy Pandian <samrpandian@gmail.com>
2021-09-14 19:36:26 -0500

    hash function add

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express") ;
 const bodyParser = require("body-parser") ;
 const ejs = require("ejs") ;
 const mongoose = require("mongoose") ;
-const encrypt = require("mongoose-encryption") ;
+const md5 = require("md5") ;
 
 const app = express() ;
 
@@ -20,14 +20,12 @@ const userSchema = new mongoose.Schema({
   password: String
 }) ;
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]}) ; // should be done before it is modeled
-
 const User = new mongoose.model("User", userSchema) ;
 
 app.post("/register", (req, res)=>{
   const newUser = new User( {
     user: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   }) ;
 
   newUser.save(function(err){
@@ -43,7 +41,7 @@ app.post("/login", (req,res)=>{
   User.findOne({user: req.body.username}, function(err, foundUser){
     if(!err) {
       if(foundUser) {
-        if(foundUser.password === req.body.password) {
+        if(foundUser.password === md5(req.body.password)) {
           res.render("secrets") ;
         } else {
           res.send("Incorrect username and/or password. Try again!")
